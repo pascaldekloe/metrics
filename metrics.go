03dcf904c586f476521bc009f1d1a38916fc5753
Pascal S. de Kloe <pascal@quies.net>
2019-02-02 23:53:52 +0100

    Gauge with real numbers.

--- metrics.go
+++ metrics.go
@@ -2,6 +2,7 @@
 package metrics
 
 import (
+	"math"
 	"net/http"
 	"strconv"
 	"strings"
@@ -10,15 +11,26 @@ import (
 	"time"
 )
 
+// Special Comments
 const (
-	helpPrefix = "# HELP "
-	typePrefix = "# TYPE "
+	helpPrefix      = "# HELP "
+	typePrefix      = "# TYPE "
+	gaugeTypeTail   = " gauge\n"
+	counterTypeTail = " counter\n"
 )
 
 // Gauge is a metric that represents a single numerical value that can
 // arbitrarily go up and down.
 type Gauge struct {
 	value int64
+	head  string
+	help  []byte
+}
+
+// RealGauge is a Gauge, but for real numbers instead of integers.
+// arbitrarily go up and down.
+type RealGauge struct {
+	value uint64
 	head  string
 	help  []byte
 }
@@ -34,24 +46,58 @@ type Counter struct {
 
 // Set updates the value.
 // Multiple goroutines may invoke this method simultaneously.
-func (g *Gauge) Set(update int64) { atomic.StoreInt64(&g.value, update) }
+func (g *Gauge) Set(update int64) {
+	atomic.StoreInt64(&g.value, update)
+}
+
+// Set updates the value.
+// Multiple goroutines may invoke this method simultaneously.
+func (g *RealGauge) Set(update float64) {
+	atomic.StoreUint64(&g.value, math.Float64bits(update))
+}
+
+// Add sets the value to the sum of the current value and summand.
+// Multiple goroutines may invoke this method simultaneously.
+func (g *Gauge) Add(summand int64) {
+	atomic.AddInt64(&g.value, summand)
+}
+
+// Add sets the value to the sum of the current value and summand.
+// Multiple goroutines may invoke this method simultaneously.
+func (g *RealGauge) Add(summand float64) {
+	for {
+		current := atomic.LoadUint64(&g.value)
+		update := math.Float64bits(math.Float64frombits(current) + summand)
+		if atomic.CompareAndSwapUint64(&g.value, current, update) {
+			return
+		}
+	}
+}
 
 // Add increments the value with diff.
 // Multiple goroutines may invoke this method simultaneously.
-func (g *Gauge) Add(diff int64) { atomic.AddInt64(&g.value, diff) }
+func (c *Counter) Add(diff uint64) {
+	atomic.AddUint64(&c.value, diff)
+}
 
-// Add increments the value with diff.
-// Multiple goroutines may invoke this method simultaneously.
-func (c *Counter) Add(diff uint64) { atomic.AddUint64(&c.value, diff) }
+func (g *Gauge) name() string {
+	return g.head[strings.LastIndexByte(g.head, '\n')+1 : len(g.head)-1]
+}
 
-func (g *Gauge) name() string   { return g.head[strings.LastIndexByte(g.head, '\n')+1 : len(g.head)-1] }
-func (c *Counter) name() string { return c.head[strings.LastIndexByte(c.head, '\n')+1 : len(c.head)-1] }
+func (g *RealGauge) name() string {
+	return g.head[strings.LastIndexByte(g.head, '\n')+1 : len(g.head)-1]
+}
+
+func (c *Counter) name() string {
+	return c.head[strings.LastIndexByte(c.head, '\n')+1 : len(c.head)-1]
+}
 
 var (
-	mutex    sync.Mutex
-	indices  = make(map[string]uint32)
-	gauges   []*Gauge
-	counters []*Counter
+	mutex      sync.Mutex
+	indices    = make(map[string]uint32)
+	gauges     []*Gauge
+	realGauges []*RealGauge
+	counters   []*Counter
 )
 
 // MustPlaceGauge registers a new Gauge if name hasn't been used before.
@@ -59,14 +105,7 @@ var (
 // name does not match regular expression [a-zA-Z_:][a-zA-Z0-9_:]*.
 func MustPlaceGauge(name string) *Gauge {
 	mustValidName(name)
-
-	var head strings.Builder
-	head.Grow(15 + 2*len(name))
-	head.WriteString(typePrefix)
-	head.WriteString(name)
-	head.WriteString(" gauge\n")
-	head.WriteString(name)
-	head.WriteByte(' ')
+	head := formatGaugeHead(name)
 
 	mutex.Lock()
 
@@ -77,7 +116,7 @@ func MustPlaceGauge(name string) *Gauge {
 		}
 		g = gauges[index]
 	} else {
-		g = &Gauge{head: head.String()}
+		g = &Gauge{head: head}
 		indices[name] = uint32(len(gauges))
 		gauges = append(gauges, g)
 	}
@@ -85,6 +124,43 @@ func MustPlaceGauge(name string) *Gauge {
 	mutex.Unlock()
 
 	return g
+}
+
+// MustPlaceRealGauge registers a new RealGauge if name hasn't been used before.
+// The function panics when name is in use as onther metric type or when
+// name does not match regular expression [a-zA-Z_:][a-zA-Z0-9_:]*.
+func MustPlaceRealGauge(name string) *RealGauge {
+	mustValidName(name)
+	head := formatGaugeHead(name)
+
+	mutex.Lock()
+
+	var g *RealGauge
+	if index, ok := indices[name]; ok {
+		if int(index) >= len(realGauges) || realGauges[index].name() != name {
+			panic("metrics: name in use as another type")
+		}
+		g = realGauges[index]
+	} else {
+		g = &RealGauge{head: head}
+		indices[name] = uint32(len(realGauges))
+		realGauges = append(realGauges, g)
+	}
+
+	mutex.Unlock()
+
+	return g
+}
+
+func formatGaugeHead(name string) string {
+	var buf strings.Builder
+	buf.Grow(15 + 2*len(name))
+	buf.WriteString(typePrefix)
+	buf.WriteString(name)
+	buf.WriteString(gaugeTypeTail)
+	buf.WriteString(name)
+	buf.WriteByte(' ')
+	return buf.String()
 }
 
 // MustPlaceCounter registers a new Counter if name hasn't been used before.
@@ -97,7 +173,7 @@ func MustPlaceCounter(name string) *Counter {
 	head.Grow(17 + 2*len(name))
 	head.WriteString(typePrefix)
 	head.WriteString(name)
-	head.WriteString(" counter\n")
+	head.WriteString(counterTypeTail)
 	head.WriteString(name)
 	head.WriteByte(' ')
 
@@ -218,10 +294,12 @@ func HTTPHandler(w http.ResponseWriter, r *http.Request) {
 
 	mutex.Lock()
 	gaugesView := gauges[:]
+	realGaugesView := realGauges[:]
 	countersView := counters[:]
 	mutex.Unlock()
 
 	timeTail = appendTimeTail(timeTail)
+
 	for _, g := range gaugesView {
 		buf = append(buf, g.head...)
 		buf = strconv.AppendInt(buf, atomic.LoadInt64(&g.value), 10)
@@ -233,7 +311,17 @@ func HTTPHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	timeTail = appendTimeTail(timeTail[:1])
+	for _, g := range realGaugesView {
+		buf = append(buf, g.head...)
+		buf = strconv.AppendFloat(buf, math.Float64frombits(atomic.LoadUint64(&g.value)), 'g', -1, 64)
+		buf = append(buf, timeTail...)
+		if len(buf) > 3900 {
+			w.Write(buf)
+			buf = buf[:0]
+			timeTail = appendTimeTail(timeTail[:1])
+		}
+	}
+
 	for _, c := range countersView {
 		buf = append(buf, c.head...)
 		buf = strconv.AppendUint(buf, atomic.LoadUint64(&c.value), 10)
